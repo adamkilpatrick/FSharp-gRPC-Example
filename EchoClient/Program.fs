8984c3df374aea5f7adff8d1065b13be10c40164
Adam Kilpatrick <akilpatrick@mi7.io>
2020-02-17 20:47:46 +0000

    Add project files.

---
+++ EchoClient/Program.fs
@@ -0,0 +1,14 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Echo
+
+[<EntryPoint>]
+let main argv =
+    AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+    let channelUrl = Environment.GetEnvironmentVariable("CHANNELURL")
+    let channel = Grpc.Net.Client.GrpcChannel.ForAddress(channelUrl)
+    let client = Echo.EchoService.EchoServiceClient(channel)
+    client.Echo(EchoMessage(MessageText="Fsharp GRPC"))
+    |> printfn "%A"
+    0
---
+++ EchoServer/Program.fs
@@ -0,0 +1,27 @@
+﻿open System
+open Echo
+
+
+type EchoServer() =
+    inherit EchoService.EchoServiceBase()
+    override _.Echo(message, context) =
+        async {
+            let reply = EchoReply(MessageResponse="Hello "+message.MessageText)
+            return reply
+        }
+        |> Async.StartAsTask
+
+[<EntryPoint>]
+let main argv =
+    let portEnv = Environment.GetEnvironmentVariable("PORT")
+    let port = int(if String.IsNullOrEmpty(portEnv) then "5000" else portEnv )
+    let server = Grpc.Core.Server()
+    server.Services.Add(EchoService.BindService(EchoServer()))
+    server.Ports.Add("0.0.0.0",port, Grpc.Core.ServerCredentials.Insecure) |> ignore
+    
+    server.Start()
+    printfn "Server listening on port %d" port
+
+    Async.Sleep(-1)
+    |> Async.RunSynchronously
+    0
